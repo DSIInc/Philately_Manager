5b7db274872b31f01ee5962cdde31473843f0ef0
DSI <kovpak.denys@gmail.com>
2021-03-23 13:27:27 +0200

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,284 @@
+﻿
+namespace Philately_Manager
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.действиеToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.оПрограммеToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.выходToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.загрузитьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.добавитьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.удалитьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.обновитьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.id = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.country = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.cost = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.year = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.circulation = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.features = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.actionsBlock = new System.Windows.Forms.GroupBox();
+            this.addButton = new System.Windows.Forms.Button();
+            this.deleteButton = new System.Windows.Forms.Button();
+            this.updateButton = new System.Windows.Forms.Button();
+            this.downloadButton = new System.Windows.Forms.Button();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.actionsBlock.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.действиеToolStripMenuItem,
+            this.оПрограммеToolStripMenuItem,
+            this.выходToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(873, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // действиеToolStripMenuItem
+            // 
+            this.действиеToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.загрузитьToolStripMenuItem,
+            this.добавитьToolStripMenuItem,
+            this.удалитьToolStripMenuItem,
+            this.обновитьToolStripMenuItem});
+            this.действиеToolStripMenuItem.Name = "действиеToolStripMenuItem";
+            this.действиеToolStripMenuItem.Size = new System.Drawing.Size(70, 20);
+            this.действиеToolStripMenuItem.Text = "Действие";
+            // 
+            // оПрограммеToolStripMenuItem
+            // 
+            this.оПрограммеToolStripMenuItem.Name = "оПрограммеToolStripMenuItem";
+            this.оПрограммеToolStripMenuItem.Size = new System.Drawing.Size(94, 20);
+            this.оПрограммеToolStripMenuItem.Text = "О программе";
+            this.оПрограммеToolStripMenuItem.Click += new System.EventHandler(this.оПрограммеToolStripMenuItem_Click);
+            // 
+            // выходToolStripMenuItem
+            // 
+            this.выходToolStripMenuItem.Name = "выходToolStripMenuItem";
+            this.выходToolStripMenuItem.Size = new System.Drawing.Size(54, 20);
+            this.выходToolStripMenuItem.Text = "Выход";
+            this.выходToolStripMenuItem.Click += new System.EventHandler(this.выходToolStripMenuItem_Click);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // загрузитьToolStripMenuItem
+            // 
+            this.загрузитьToolStripMenuItem.Name = "загрузитьToolStripMenuItem";
+            this.загрузитьToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.загрузитьToolStripMenuItem.Text = "Загрузить";
+            // 
+            // добавитьToolStripMenuItem
+            // 
+            this.добавитьToolStripMenuItem.Name = "добавитьToolStripMenuItem";
+            this.добавитьToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.добавитьToolStripMenuItem.Text = "Добавить";
+            // 
+            // удалитьToolStripMenuItem
+            // 
+            this.удалитьToolStripMenuItem.Name = "удалитьToolStripMenuItem";
+            this.удалитьToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.удалитьToolStripMenuItem.Text = "Удалить";
+            // 
+            // обновитьToolStripMenuItem
+            // 
+            this.обновитьToolStripMenuItem.Name = "обновитьToolStripMenuItem";
+            this.обновитьToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.обновитьToolStripMenuItem.Text = "Обновить";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.id,
+            this.country,
+            this.cost,
+            this.year,
+            this.circulation,
+            this.features});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 27);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(646, 248);
+            this.dataGridView1.TabIndex = 2;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // id
+            // 
+            this.id.HeaderText = "ID";
+            this.id.Name = "id";
+            // 
+            // country
+            // 
+            this.country.HeaderText = "Країна";
+            this.country.Name = "country";
+            // 
+            // cost
+            // 
+            this.cost.HeaderText = "Вартість";
+            this.cost.Name = "cost";
+            // 
+            // year
+            // 
+            this.year.HeaderText = "Рік";
+            this.year.Name = "year";
+            // 
+            // circulation
+            // 
+            this.circulation.HeaderText = "Тираж";
+            this.circulation.Name = "circulation";
+            // 
+            // features
+            // 
+            this.features.HeaderText = "Особливості";
+            this.features.Name = "features";
+            // 
+            // actionsBlock
+            // 
+            this.actionsBlock.Controls.Add(this.downloadButton);
+            this.actionsBlock.Controls.Add(this.updateButton);
+            this.actionsBlock.Controls.Add(this.deleteButton);
+            this.actionsBlock.Controls.Add(this.addButton);
+            this.actionsBlock.Location = new System.Drawing.Point(664, 28);
+            this.actionsBlock.Name = "actionsBlock";
+            this.actionsBlock.Size = new System.Drawing.Size(197, 248);
+            this.actionsBlock.TabIndex = 3;
+            this.actionsBlock.TabStop = false;
+            this.actionsBlock.Text = "Дії";
+            // 
+            // addButton
+            // 
+            this.addButton.Location = new System.Drawing.Point(6, 92);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(185, 23);
+            this.addButton.TabIndex = 0;
+            this.addButton.Text = "Добавить";
+            this.addButton.UseVisualStyleBackColor = true;
+            this.addButton.Click += new System.EventHandler(this.addButton_Click);
+            // 
+            // deleteButton
+            // 
+            this.deleteButton.Location = new System.Drawing.Point(6, 121);
+            this.deleteButton.Name = "deleteButton";
+            this.deleteButton.Size = new System.Drawing.Size(185, 23);
+            this.deleteButton.TabIndex = 1;
+            this.deleteButton.Text = "Удалить";
+            this.deleteButton.UseVisualStyleBackColor = true;
+            this.deleteButton.Click += new System.EventHandler(this.deleteButton_Click);
+            // 
+            // updateButton
+            // 
+            this.updateButton.Location = new System.Drawing.Point(6, 150);
+            this.updateButton.Name = "updateButton";
+            this.updateButton.Size = new System.Drawing.Size(185, 23);
+            this.updateButton.TabIndex = 2;
+            this.updateButton.Text = "Обновить";
+            this.updateButton.UseVisualStyleBackColor = true;
+            this.updateButton.Click += new System.EventHandler(this.updateButton_Click);
+            // 
+            // downloadButton
+            // 
+            this.downloadButton.Location = new System.Drawing.Point(6, 63);
+            this.downloadButton.Name = "downloadButton";
+            this.downloadButton.Size = new System.Drawing.Size(185, 23);
+            this.downloadButton.TabIndex = 4;
+            this.downloadButton.Text = "Загрузить";
+            this.downloadButton.UseVisualStyleBackColor = true;
+            this.downloadButton.Click += new System.EventHandler(this.downloadButton_Click);
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(415, 449);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(240, 150);
+            this.dataGridView2.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(873, 302);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.actionsBlock);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Philately Manager v1.0";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.actionsBlock.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem действиеToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem загрузитьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem добавитьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem удалитьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem обновитьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem оПрограммеToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem выходToolStripMenuItem;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn id;
+        private System.Windows.Forms.DataGridViewTextBoxColumn country;
+        private System.Windows.Forms.DataGridViewTextBoxColumn cost;
+        private System.Windows.Forms.DataGridViewTextBoxColumn year;
+        private System.Windows.Forms.DataGridViewTextBoxColumn circulation;
+        private System.Windows.Forms.DataGridViewTextBoxColumn features;
+        private System.Windows.Forms.GroupBox actionsBlock;
+        private System.Windows.Forms.Button updateButton;
+        private System.Windows.Forms.Button deleteButton;
+        private System.Windows.Forms.Button addButton;
+        private System.Windows.Forms.Button downloadButton;
+        private System.Windows.Forms.DataGridView dataGridView2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+namespace Philately_Manager
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void addButton_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count != 1)
+            {
+                MessageBox.Show("Виберіть 1 строку!","Увага!");
+            }
+            //Запам'ятаємо вибрану строку
+            int index = dataGridView1.SelectedRows[0].Index;
+
+            //Перевіримо дані з таблиці
+            if (dataGridView1.Rows[index].Cells[0].Value == null ||
+                dataGridView1.Rows[index].Cells[1].Value == null ||
+                dataGridView1.Rows[index].Cells[2].Value == null ||
+                dataGridView1.Rows[index].Cells[3].Value == null ||
+                dataGridView1.Rows[index].Cells[4].Value == null ||
+                dataGridView1.Rows[index].Cells[5].Value == null)
+            {
+                MessageBox.Show("Не всі дані введені!", "Увага!");
+                return;
+            }
+
+            //Зчитаємо дані
+            string id = dataGridView1.Rows[index].Cells[0].Value.ToString();
+            string country = dataGridView1.Rows[index].Cells[1].Value.ToString();
+            string cost = dataGridView1.Rows[index].Cells[2].Value.ToString();
+            string year = dataGridView1.Rows[index].Cells[3].Value.ToString();
+            string circularity = dataGridView1.Rows[index].Cells[4].Value.ToString();
+            string features = dataGridView1.Rows[index].Cells[5].Value.ToString();
+
+            //Створюємо з'єднанання
+            string connectionString = "provider=Microsoft.Jet.OLEDB.4.0;Data Source=Database1.mdb"; //строка з'єднання
+            OleDbConnection dbConnection = new OleDbConnection(connectionString); //створюємо з'єднання
+
+            //Виконуємо запрос до БД
+            dbConnection.Open(); //Відкриваємо з'єднання
+            string query = "INSERT INTO Marks VALUES (" + id + ", '" + country + "', " + cost + ", " + year + ", " + circularity + ", '" + features + "')"; //Строка запроса
+            OleDbCommand dbCommand = new OleDbCommand(query, dbConnection);
+
+            if (dbCommand.ExecuteNonQuery() != 1)
+                MessageBox.Show("Помилка виконання запросу!", "Помилка!");
+            else
+                MessageBox.Show("Дані добавлені!", "Успіх!");
+
+            //Закриваємо з'єднання
+            dbConnection.Close();
+        }
+
+        private void deleteButton_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count != 1)
+            {
+                MessageBox.Show("Виберіть 1 строку!", "Увага!");
+            }
+            //Запам'ятаємо вибрану строку
+            int index = dataGridView1.SelectedRows[0].Index;
+
+            //Перевіримо дані з таблиці
+            if (dataGridView1.Rows[index].Cells[0].Value == null)
+            {
+                MessageBox.Show("Не всі дані введені!", "Увага!");
+                return;
+            }
+
+            //Зчитаємо дані
+            string id = dataGridView1.Rows[index].Cells[0].Value.ToString();
+
+            //Створюємо з'єднанання
+            string connectionString = "provider=Microsoft.Jet.OLEDB.4.0;Data Source=Database1.mdb"; //строка з'єднання
+            OleDbConnection dbConnection = new OleDbConnection(connectionString); //створюємо з'єднання
+
+            //Виконуємо запрос до БД
+            dbConnection.Open(); //Відкриваємо з'єднання
+            string query = "DELETE FROM Marks WHERE id = "+ id +""; //Строка запроса
+            OleDbCommand dbCommand = new OleDbCommand(query, dbConnection);
+
+            if (dbCommand.ExecuteNonQuery() != 1)
+                MessageBox.Show("Помилка виконання запросу!", "Помилка!");
+            else
+            {
+                MessageBox.Show("Дані видалені!", "Успіх!");
+                //Видаляємо дані з таблиці в формі
+                dataGridView1.Rows.RemoveAt(index);
+            }
+
+            //Закриваємо з'єднання
+            dbConnection.Close();
+        }
+
+        private void updateButton_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count != 1)
+            {
+                MessageBox.Show("Виберіть 1 строку!", "Увага!");
+            }
+            //Запам'ятаємо вибрану строку
+            int index = dataGridView1.SelectedRows[0].Index;
+
+            //Перевіримо дані з таблиці
+            if (dataGridView1.Rows[index].Cells[0].Value == null ||
+                dataGridView1.Rows[index].Cells[1].Value == null ||
+                dataGridView1.Rows[index].Cells[2].Value == null ||
+                dataGridView1.Rows[index].Cells[3].Value == null ||
+                dataGridView1.Rows[index].Cells[4].Value == null ||
+                dataGridView1.Rows[index].Cells[5].Value == null)
+            {
+                MessageBox.Show("Не всі дані введені!", "Увага!");
+                return;
+            }
+
+            //Зчитаємо дані
+            string id = dataGridView1.Rows[index].Cells[0].Value.ToString();
+            string country = dataGridView1.Rows[index].Cells[1].Value.ToString();
+            string cost = dataGridView1.Rows[index].Cells[2].Value.ToString();
+            string year = dataGridView1.Rows[index].Cells[3].Value.ToString();
+            string circularity = dataGridView1.Rows[index].Cells[4].Value.ToString();
+            string features = dataGridView1.Rows[index].Cells[5].Value.ToString();
+
+            //Створюємо з'єднанання
+            string connectionString = "provider=Microsoft.Jet.OLEDB.4.0;Data Source=Database1.mdb"; //строка з'єднання
+            OleDbConnection dbConnection = new OleDbConnection(connectionString); //створюємо з'єднання
+
+            //Виконуємо запрос до БД
+            dbConnection.Open(); //Відкриваємо з'єднання
+            string query = "UPDATE Marks SET Id = " + id + ", Country = '" + country + "', Cost = " + cost + ", Year = " + year + ", Circularity = " + circularity + ", Features ='" + features +"'"; //Строка запроса
+            OleDbCommand dbCommand = new OleDbCommand(query, dbConnection);
+
+            if (dbCommand.ExecuteNonQuery() != 1)
+                MessageBox.Show("Помилка виконання запросу!", "Помилка!");
+            else
+                MessageBox.Show("Дані обновлені!", "Успіх!");
+
+            //Закриваємо з'єднання
+            dbConnection.Close();
+        }
+
+        private void выходToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void оПрограммеToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Курсова робота студента групи КБ-20-1\n Ковпака Дениса", "Philately Manager");
+        }
+
+        private void downloadButton_Click(object sender, EventArgs e)
+        {
+            //Створюємо з'єднанання
+            string connectionString = "provider=Microsoft.Jet.OLEDB.4.0;Data Source=Database1.mdb"; //строка з'єднання
+            OleDbConnection dbConnection = new OleDbConnection(connectionString); //створюємо з'єднання
+
+            //Виконуємо запрос до БД
+            dbConnection.Open(); //Відкриваємо з'єднання
+            string query = "SELECT * FROM Marks"; //Строка запроса
+            OleDbCommand dbCommand = new OleDbCommand(query, dbConnection);
+            OleDbDataReader dbReader = dbCommand.ExecuteReader();
+
+            //Перевіримо дані
+            if(dbReader.HasRows == false)
+            {
+                MessageBox.Show("Дані не знайдено!", "Помилка!");
+            }
+            else
+            {
+                while (dbReader.Read())
+                {
+                    dataGridView1.Rows.Add(dbReader["m_code"], dbReader["m_country"], dbReader["m_cost"], dbReader["m_year"], dbReader["m_circulation"], dbReader["m_features"]);
+                }
+            }
+
+            //Закриваємо з'єднання
+            dbReader.Close();
+            dbConnection.Close();
+        }
+    }
+}
